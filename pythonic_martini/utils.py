6666f0f5228ca6c4030506923942dce80a816934
Mayank Agrawal <amayank@umich.edu>
2020-10-30 16:05:48 -0500

    utils now have the functions actual_molecules_added and actual_atoms_added

--- pythonic_martini/utils.py
+++ pythonic_martini/utils.py
@@ -6,6 +6,66 @@ from . import quaternion
 
 
 
+def actual_atoms_added(filename, keyword):
+    """Count the number of lines with <keyword> in the <filename> - usually gro or pdb file
+    """
+    num_keyword = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            if keyword in line:
+                num_keyword += 1
+
+    return num_keyword
+
+
+
+
+def actual_molecules_added(filename, itpfilename, start_linenumber=0):
+    """Calculate the number of molecules, defined in <itpfilename>, 
+    in <filename> - usually gro or pdb file
+    """
+    
+    # get atom names from the <molname>.itp
+    atom_names=[]
+    start=False
+    with open(itpfilename, 'r') as f:
+        for line in f:
+            if not start:
+                if '[ atoms ]' in line:
+                    start = True
+                    continue
+            if start:
+                if line.split()==[]:
+                    start = False
+                    break
+                atom_names += [line.split()[3]]
+      
+    # get all the atom names from the <outfilename>
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    all_names = [] # e.g. PAM, ALA, GLU
+    for line in lines[start_linenumber:]:
+        if line.split()==[]:
+            continue
+        all_names += [line.split()[0].lstrip('0123456789')]
+
+    # now count number of atom_names sequence in all_names
+    actual_nmol = 0
+    natoms = len(atom_names)
+    i = 0
+    while i<len(all_names):
+        if all_names[i:i+natoms] == atom_names:
+            i += natoms
+            actual_nmol += 1
+        else:
+            i += 1
+
+
+    return actual_nmol
+
+
+
+
 def make_molecule_template(mol_names, mol_numbers, atom_names, atom_positions, filename):
     '''Create the molecule scaffold structure to be used by VMD to make the all-atom structure
     molecules: list of [molecule name, position list]
